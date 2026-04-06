c737f42301fa9b24ac786ae5fcf8a976c25292ec
Paola <montserrat_osorio@yahoo.com>
2026-04-05 21:32:39 -0600

    feat: update TomyService base URL to production and add http dependency

--- lib/services/tomy_service.dart
+++ lib/services/tomy_service.dart
@@ -1,20 +1,9 @@
 import 'dart:convert';
-import 'dart:io';
-import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import '../models/chat_message.dart';
 
 class TomyService {
-  static String get baseUrl {
-    if (kIsWeb) {
-      return 'http://localhost:8000';
-    } else if (Platform.isAndroid) {
-      return 'http://10.0.2.2:8000';
-    } else if (Platform.isWindows || Platform.isMacOS || Platform.isLinux) {
-      return 'http://localhost:8000';
-    }
-    return 'http://localhost:8000';
-  }
+  static const String baseUrl = 'https://tomy-production.up.railway.app';
 
   static Future<ChatMessage> sendMessage({
     required String userId,
